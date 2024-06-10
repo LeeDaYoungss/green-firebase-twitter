0ab135f2ef59a30732ba1dd8a0b5062b0847dbed
LeeDaYoungss <leedayeonggg@gmail.com>
2024-06-10 15:17:10 +0900

    프로필페이지생성

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,16 +2,24 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Auth from "./Auth";
 import Home from "../routes/Home";
+import Nav from "./Nav";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({isLoggedIn})=>{
   
   return(
-    <Routes>
-      {isLoggedIn ? 
-        <Route path="/" element={<Home/>}></Route>
-        :<Route path="/" element={<Auth/>}></Route>
-      }
-    </Routes>
+    <>
+      {isLoggedIn && <Nav/>}
+      <Routes>
+        {isLoggedIn ? 
+          <>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+          </>
+          :<Route path="/" element={<Auth/>}></Route>
+        }
+      </Routes>
+    </>
   )
 }
 
