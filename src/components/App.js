7441755bbac4c842fa68ad59dbaeef03efae3f89
LeeDaYoungss <leedayeonggg@gmail.com>
2024-06-10 13:17:09 +0900

    로그인 출력

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
 import '../App.css';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from './Router';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const[isLoggedIn, setIsloggedIn] = useState(false);
-  return <AppRouter isLoggedIn={isLoggedIn}/>;
-}
+  const[init, setInit] = useState(false);
+  const auth = getAuth();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user)=>{
+      if (user) {
+        // const uid = user.uid;
+        setIsloggedIn(true);
+      } else {
+        // user is signed out.
+        setIsloggedIn(false);
+      }
+      setInit(true);
+    })
+  },[]);
+
+  return (
+    <>
+      {init ?
+        <AppRouter isLoggedIn={isLoggedIn}/>
+        :"Intializing..."
+      }
+    </>
+  )
+};
+  
 
 export default App;
