98cfbaa9e3a96bbc19c4fe4a5dcee5fe36a50031
LeeDaYoungss <leedayeonggg@gmail.com>
2024-06-10 13:16:58 +0900

    에러메세지 출력

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -7,6 +7,7 @@ const Auth = ()=> {
   const[email, setEmail] = useState('');
   const[password, setPassword] = useState('');
   const[newAccount, setNewAccount] = useState(true);
+  const[error, setError] = useState('');
   const auth = getAuth();
 
   const onChange = (e)=>{
@@ -34,26 +35,41 @@ const Auth = ()=> {
           // Signed in 
           const user = userCredential.user;
           console.log(user);
-          // ...
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
           console.log(errorCode, errorMessage);
-          // ..
+          setError(errorMessage);
         });
     }else{
       // 로그인
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(errorMessage);
+      });
     }
   }
   console.log(email, password);
-
+  const toggleAccount = () => setNewAccount(prev=>!prev);
   return(
-    <form onSubmit={onSubmit}>
-      <input type="email" name="email" placeholder="Email 입력하세요" onChange={onChange} />
-      <input type="password" name="password" placeholder="비밀번호를 입력하세요" onChange={onChange} />
-      <p><button>{newAccount ? '회원가입': ' 로그인'}로그인</button></p>
-    </form>
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="email" name="email" placeholder="Email 입력하세요" onChange={onChange} />
+        <input type="password" name="password" placeholder="비밀번호를 입력하세요" onChange={onChange} />
+        <p><button>{newAccount ? '회원가입': ' 로그인'}</button></p>
+        {error}
+      </form>
+      <hr/>
+      <button type="button" onClick={toggleAccount}>{newAccount ? '로그인':'회원가입'}</button>
+    </>
   )
 }
 export default Auth;
